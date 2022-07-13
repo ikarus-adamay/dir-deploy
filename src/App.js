9d92ce3809c08d9d96e76c38e4ffdaad62fc57d4
Adamay Mann <meadamann2002@gmail.com>
2022-07-13 16:22:51 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import SignUp from "./components/SignUp";
 
 const App = () => {
   return (
     <>
       <Navbar/>
-      <SignUp/>
     </>
   );
 };
